8464bb4362023fa223dc41117aa80c783d65dacb
Jhoni Faria <jhonifarias16@gmail.com>
2024-12-16 12:56:36 -0300

    feat: add cache in find products

--- src/application/use-cases/product/find-product.use-case.ts
+++ src/application/use-cases/product/find-product.use-case.ts
@@ -1,23 +1,36 @@
 import { ProductResponseDto } from '@Application/dtos/response/product/product.response.dto';
+import { ProductMapper } from '@Application/mappers/product.mapper';
 import {
   IProductService,
   IProductServiceSymbol,
 } from '@Domain/services/product/product.service';
+import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
+
 import { Inject, Injectable } from '@nestjs/common';
-import { ProductMapper } from '../../mappers/product.mapper';
 
 @Injectable()
 export class FindProductUseCase {
   constructor(
     @Inject(IProductServiceSymbol)
     private readonly productService: IProductService,
+    @Inject(CACHE_MANAGER) private cacheService: Cache,
   ) {}
 
   async execute(
     name: string,
     categoryId: number,
   ): Promise<ProductResponseDto[]> {
+    const cachedData = await this.cacheService.get<string>('products');
+
+    if (cachedData) {
+      return JSON.parse(cachedData);
+    }
+
     const products = await this.productService.findProducts(name, categoryId);
-    return products.map(ProductMapper.toResponseDto);
+    const productResponseDto = products.map(ProductMapper.toResponseDto);
+
+    await this.cacheService.set('products', JSON.stringify(productResponseDto));
+
+    return productResponseDto;
   }
 }
